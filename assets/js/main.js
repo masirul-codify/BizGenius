28578e205b4589a1e15d7a22ee6df2a2700bb42f
Masirul Islam <masirulislam80@gmail.com>
2023-10-29 23:08:55 +0600

    faq add

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,36 +32,36 @@
 
 ****************************************************/
 
-(function($) {
-	"use strict"
+// faq area start
 
-	var windowOn = $(window);
-	
-	// 00. PreLoader
-	function loader() {
-		$(window).on('load', function () {
-			$('#ctn-preloader').addClass('loaded');
-			$("#loading").fadeOut(500);
-
-			if ($('#ctn-preloader').hasClass('loaded')) {
-				$('#preloader').delay(900).queue(function () {
-					$(this).remove();
-				});
-			}
-		});
-	}
-	loader();
-	
-	// 02. Mobile Menu
-	$("#mobile-menu").meanmenu({
-		meanMenuContainer: ".mobile-menu",
-		meanScreenWidth: "1199",
-		meanExpand: ['<i class="fal fa-plus"></i>'],
-		meanContract: ['<i class="fal fa-minus"></i>'],
-	});
-	$("#mobile-menu-media-all").meanmenu({
-		meanMenuContainer: ".mobile-menu-media-all",
-		meanScreenWidth: "8000",
-		meanExpand: ['<i class="fal fa-plus"></i>'],
-		meanContract: ['<i class="fal fa-minus"></i>'],
-	});
+		const accordionItemHeaders = document.querySelectorAll(
+			".accordion-item-header"
+		  );
+		  
+		  accordionItemHeaders.forEach((accordionItemHeader) => {
+			accordionItemHeader.addEventListener("click", (event) => {
+			  // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+		  
+			  const currentlyActiveAccordionItemHeader = document.querySelector(
+				".accordion-item-header.active"
+			  );
+			  if (
+				currentlyActiveAccordionItemHeader &&
+				currentlyActiveAccordionItemHeader !== accordionItemHeader
+			  ) {
+				currentlyActiveAccordionItemHeader.classList.toggle("active");
+				currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+			  }
+			  accordionItemHeader.classList.toggle("active");
+			  const accordionItemBody = accordionItemHeader.nextElementSibling;
+			  if (accordionItemHeader.classList.contains("active")) {
+				accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+			  } else {
+				accordionItemBody.style.maxHeight = 0;
+			  }
+			});
+		  });
+		  
+		  
+		  
+	// faq area end
